1469a89f6ccdc57fe3e1883bed53a0b989958119
Thales Agapito <thsousa101@gmail.com>
2020-07-11 16:21:02 -0300

    remove colors from theme_colors and and clean up main

--- lib/theme/theme_colors.dart
+++ lib/theme/theme_colors.dart
@@ -5,9 +5,7 @@ class ThemeColors {
   ThemeColors._();
 
   static const Color avocado = Color(0xFF3AAFA2);
-  static const Color lapis = Color(0xFF5C6AC4);
   static const Color info = Color(0xFFA8D5E2);
-  static const Color danger = Color(0xFFFE6364);
   static const Color warning = Color(0xFFFFBA49);
   static const Color success = Color(0xFF60D394);
 }
