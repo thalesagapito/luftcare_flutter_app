38e1eefe15f5672810cae1a16a52d01b74ed1802
Thales Agapito <thsousa101@gmail.com>
2020-07-15 23:11:39 -0300

    refactor login screen

--- lib/screens/guest/login_screen.dart
+++ lib/screens/guest/login_screen.dart
@@ -4,31 +4,44 @@ import 'package:luftcare_flutter_app/widgets/atoms/brand_logo.dart';
 
 class LoginScreenArgs {}
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({Key key}) : super(key: key);
+class LoginScreen extends StatefulWidget {
+  LoginScreen({Key key}) : super(key: key);
 
   static const RouteName = '/login';
 
   @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _emailFocusNode = FocusNode();
+  final _passwordFocusNode = FocusNode();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    _emailFocusNode.dispose();
+    _passwordFocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final logoHeight = 32.0;
+    const logoHeight = 32.0;
     final theme = Theme.of(context);
     final mediaQuery = MediaQuery.of(context);
-    final safeAreaVerticalPadding = mediaQuery.padding.vertical;
-    const containerPadding = EdgeInsets.all(30);
-    final containerConstraints = BoxConstraints(
-      maxHeight: mediaQuery.size.height - safeAreaVerticalPadding,
+    const pagePadding = EdgeInsets.all(30);
+    final pageConstraints = BoxConstraints(
+      maxHeight: mediaQuery.size.height - mediaQuery.padding.vertical,
       maxWidth: mediaQuery.size.width,
       minWidth: mediaQuery.size.width,
     );
-    final emailFocusNode = FocusNode();
-    final passwordFocusNode = FocusNode();
 
     return Scaffold(
       body: SafeArea(
         child: Container(
-          padding: containerPadding,
-          constraints: containerConstraints,
+          padding: pagePadding,
+          constraints: pageConstraints,
           child: LayoutBuilder(
             builder: (context, constraints) => Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -36,14 +49,18 @@ class LoginScreen extends StatelessWidget {
               children: <Widget>[
                 BrandLogo(height: logoHeight),
                 Spacer(),
-                _buildForm(
-                  constraints,
-                  logoHeight,
-                  theme,
-                  emailFocusNode,
-                  passwordFocusNode,
+                _buildFormWrapper(
+                  constraints: constraints,
+                  logoHeight: logoHeight,
+                  child: _buildForm(
+                    theme: theme,
+                    formKey: _formKey,
+                    constraints: constraints,
+                    emailFocusNode: _emailFocusNode,
+                    passwordFocusNode: _passwordFocusNode,
+                  ),
                 ),
-                _buildLoginButton(theme, () {})
+                _buildLoginButton(theme, context)
               ],
             ),
           ),
@@ -52,13 +69,11 @@ class LoginScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildForm(
+  Widget _buildFormWrapper({
     BoxConstraints constraints,
     double logoHeight,
-    ThemeData theme,
-    FocusNode emailFocusNode,
-    FocusNode passwordFocusNode,
-  ) {
+    Widget child,
+  }) {
     const topPadding = 20;
     const scrollViewPadding = EdgeInsets.only(bottom: 20);
     final containerMaxHeight = constraints.maxHeight - logoHeight - topPadding;
@@ -68,52 +83,68 @@ class LoginScreen extends StatelessWidget {
       constraints: BoxConstraints(maxHeight: containerMaxHeight),
       child: SingleChildScrollView(
         padding: scrollViewPadding,
-        child: Form(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              _buildTitle(theme),
-              _buildLoginTextField(
-                theme: theme,
-                constraints: constraints,
-                icon: CupertinoIcons.mail,
-                child: TextFormField(
-                  focusNode: emailFocusNode,
-                  keyboardAppearance: theme.brightness,
-                  textInputAction: TextInputAction.next,
+        child: child,
+      ),
+    );
+  }
 
-                  onFieldSubmitted: (_) => passwordFocusNode.requestFocus(),
-                  keyboardType: TextInputType.emailAddress,
-                  // focusNode: ,
-                  decoration: InputDecoration(
-                    labelText: 'Endereço de email',
-                    focusColor: theme.primaryColor,
-                    border: OutlineInputBorder(),
-                  ),
-                ),
+  Widget _buildForm({
+    @required ThemeData theme,
+    @required GlobalKey formKey,
+    @required FocusNode emailFocusNode,
+    @required FocusNode passwordFocusNode,
+    @required BoxConstraints constraints,
+  }) {
+    return Form(
+      key: formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          _buildTitle(theme),
+          _buildLoginTextField(
+            theme: theme,
+            constraints: constraints,
+            icon: CupertinoIcons.mail,
+            child: TextFormField(
+              focusNode: emailFocusNode,
+              keyboardAppearance: theme.brightness,
+              textInputAction: TextInputAction.next,
+              onFieldSubmitted: (_) => passwordFocusNode.requestFocus(),
+              keyboardType: TextInputType.emailAddress,
+              // focusNode: ,
+              decoration: InputDecoration(
+                labelText: 'Endereço de email',
+                focusColor: theme.primaryColor,
+                border: OutlineInputBorder(),
               ),
-              SizedBox(height: 20),
-              _buildLoginTextField(
-                theme: theme,
-                constraints: constraints,
-                icon: CupertinoIcons.padlock,
-                child: TextFormField(
-                  obscureText: true,
-                  focusNode: passwordFocusNode,
-                  textInputAction: TextInputAction.done,
-                  keyboardAppearance: theme.brightness,
-                  decoration: InputDecoration(
-                    labelText: 'Senha',
-                    focusColor: theme.primaryColor,
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-              ),
-              SizedBox(height: 10),
-            ],
+            ),
           ),
-        ),
+          SizedBox(height: 20),
+          _buildLoginTextField(
+            theme: theme,
+            constraints: constraints,
+            icon: CupertinoIcons.padlock,
+            child: TextFormField(
+              obscureText: true,
+              focusNode: passwordFocusNode,
+              textInputAction: TextInputAction.done,
+              keyboardAppearance: theme.brightness,
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Obrigatório';
+                }
+                return null;
+              },
+              decoration: InputDecoration(
+                labelText: 'Senha',
+                focusColor: theme.primaryColor,
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+        ],
       ),
     );
   }
@@ -160,16 +191,17 @@ class LoginScreen extends StatelessWidget {
       width: double.infinity,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[sideIcon, constrainedChild],
       ),
     );
   }
 
-  Widget _buildLoginButton(ThemeData theme, Function pressedHandler) {
+  Widget _buildLoginButton(ThemeData theme, BuildContext context) {
     return Container(
       width: double.infinity,
       child: FlatButton(
-        onPressed: pressedHandler,
+        onPressed: () => _validateAndSubmitForm(context),
         color: theme.primaryColor,
         child: const Text(
           'Entrar',
@@ -177,5 +209,13 @@ class LoginScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void _validateAndSubmitForm(BuildContext context) {
+    final isFormValid = _formKey.currentState.validate();
+    if (isFormValid) {
+      Scaffold.of(context).showSnackBar(SnackBar(content: Text('beleza')));
+    }
+    // _formKey.currentState.save();
   }
 }
