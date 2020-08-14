0feb9b9971a8afa7eadeb5e5332a378e8155d77c
Thales Agapito <thsousa101@gmail.com>
2020-08-13 22:54:17 -0300

    remove login method from login screen and move it to auth provider

--- lib/screens/guest/login_screen.dart
+++ lib/screens/guest/login_screen.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
-import 'package:luftcare_flutter_app/secure_storage.dart';
+import 'package:luftcare_flutter_app/providers/auth_provider.dart';
 import 'package:luftcare_flutter_app/widgets/atoms/brand_logo.dart';
-import 'package:luftcare_flutter_app/models/graphql/api.graphql.dart';
-import 'package:luftcare_flutter_app/screens/patient/home_screen.dart';
 import 'package:luftcare_flutter_app/widgets/organisms/auth/login_form.dart';
+import 'package:provider/provider.dart';
 
 class LoginScreen extends StatelessWidget {
   static const RouteName = '/login';
@@ -13,6 +11,7 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final login = Provider.of<Auth>(context).login;
     final mediaQuery = MediaQuery.of(context);
     const pagePadding = EdgeInsets.fromLTRB(30, 30, 30, 0);
     final pageConstraints = BoxConstraints(
@@ -35,7 +34,7 @@ class LoginScreen extends StatelessWidget {
                   constraints: constraints,
                   child: LoginForm(
                     onSubmit: ({email, password}) =>
-                        _login(context, email, password),
+                        login(context, email, password),
                   ),
                 ),
               ],
@@ -64,35 +63,5 @@ class LoginScreen extends StatelessWidget {
         ),
       ),
     ];
-  }
-
-  void _login(BuildContext context, String email, String password) async {
-    final client = GraphQLProvider.of(context).value;
-    final loginArgs = LoginArguments(email: email, password: password);
-    final loginMutation = MutationOptions(
-      documentNode: LoginMutation().document,
-      variables: loginArgs.toJson(),
-    );
-
-    final QueryResult result = await client.mutate(loginMutation);
-
-    if (result.hasException) {
-      final OperationException exception = result?.exception;
-      final GraphQLError error = exception?.graphqlErrors[0];
-      final String message = error?.message ?? 'Erro interno';
-
-      Scaffold.of(context).showSnackBar(SnackBar(
-        content: Text(message),
-      ));
-      return;
-    }
-
-    final res = Login$Mutation.fromJson(result.data).login;
-    await SecureStorage().login(auth: res.authorization, refresh: res.refresh);
-
-    Navigator.of(context).pushNamedAndRemoveUntil(
-      HomeScreen.RouteName,
-      (_) => false,
-    );
   }
 }
