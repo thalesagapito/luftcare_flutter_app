280282094cad63d89b1bfdfa1b56b93277637e2f
Thales Agapito <thsousa101@gmail.com>
2020-09-18 23:21:08 -0300

    simplify answered and available questionnaires widgets with reusable overview list widget

--- lib/screens/patient/home_screen/overview/answered_questionnaires.dart
+++ lib/screens/patient/home_screen/overview/answered_questionnaires.dart
@@ -1,63 +1,150 @@
+import 'dart:html';
+
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:luftcare_flutter_app/helpers/score_colors.dart';
+import 'package:luftcare_flutter_app/models/graphql/api.graphql.dart';
+import 'package:luftcare_flutter_app/widgets/atoms/layout/rounded_list_tile_wrapper.dart';
+import 'package:luftcare_flutter_app/widgets/organisms/helpers/current_user_builder.dart';
+import 'package:luftcare_flutter_app/providers/symptom_questionnaire_response_provider.dart';
+import 'package:luftcare_flutter_app/providers/symptom_questionnaire_responses_provider.dart';
+import 'package:luftcare_flutter_app/screens/patient/home_screen/overview/overview_list.dart';
 
 class AnsweredQuestionnaires extends StatelessWidget {
-  const AnsweredQuestionnaires({Key key}) : super(key: key);
+  const AnsweredQuestionnaires({@required this.selectedDate});
+
+  final DateTime selectedDate;
+
+  DateTime _getMidnightDateTime(DateTime dateTime) =>
+      new DateTime(dateTime.year, dateTime.month, dateTime.day, 0, 0, 0);
+
+  DateTime _getBeforeMidnightDateTime(DateTime dateTime) =>
+      new DateTime(dateTime.year, dateTime.month, dateTime.day, 23, 59, 59);
+
+  String _getTrailingText(Responses$Query$SymptomQuestionnaireResponses$Results response) {
+    return DateFormat.Hm().format(response.responseDate);
+  }
+
+  _showDialog(Responses$Query$SymptomQuestionnaireResponses$Results response, BuildContext ctx) {
+    final closeText = const Text('Fechar');
+    final title = const Text('Resumo da resposta');
+    final onClose = () => Navigator.of(ctx).pop();
+    final questionsAndAnswersList = _getQuestionsAndAnswersList(response);
+
+    showDialog(
+      context: ctx,
+      builder: (_) => AlertDialog(
+        title: title,
+        scrollable: true,
+        content: Column(
+          children: questionsAndAnswersList,
+        ),
+        actions: [FlatButton(onPressed: onClose, child: closeText)],
+      ),
+    );
+  }
+
+  _getQuestionsAndAnswersList(Responses$Query$SymptomQuestionnaireResponses$Results response) {
+    return SymptomQuestionnaireResponse.getResponseSummary(response: response)
+        .map((qa) => _QuestionAndAnswerTile(questionText: qa.item1, answerText: qa.item2))
+        .toList();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final answeredQuestionnaires = [];
-    final hasNoAnsweredQuestionnaires = answeredQuestionnaires.length == 0;
+    return CurrentUserBuilder(
+      builder: (currentUser) {
+        final documentNode = SymptomQuestionnaireResponses.responsesQueryDocumentNode;
+        final queryVariables = ResponsesArguments(
+          id: currentUser.id,
+          responseDateAfter: _getMidnightDateTime(selectedDate),
+          responseDateBefore: _getBeforeMidnightDateTime(selectedDate),
+        ).toJson();
+        final queryOptions = QueryOptions(documentNode: documentNode, variables: queryVariables);
 
-    if (hasNoAnsweredQuestionnaires) return Container();
+        return Query(
+          options: queryOptions,
+          builder: (result, {fetchMore, refetch}) {
+            final response = SymptomQuestionnaireResponses.getResponsesFromQueryResult(result);
+            final questionnaireResponses = response?.results ?? [];
 
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          _Title(),
-          _AnsweredQuestionnairesList(),
-          Placeholder(fallbackHeight: 90),
-          Placeholder(fallbackHeight: 90),
-        ],
-      ),
+            final responseTiles = questionnaireResponses
+                .map(
+                  (response) => RoundedListTileWrapper(
+                    title: Text(response.questionnaire.nameForPresentation),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(_getTrailingText(response)),
+                        _ScoreBadge(response),
+                      ],
+                    ),
+                    onTap: () => _showDialog(response, context),
+                  ),
+                )
+                .toList();
+
+            return OverviewList(
+              title: 'Questionários respondidos',
+              children: responseTiles,
+              hasError: result.hasException,
+              isLoading: result.loading,
+              errorText: 'Ocorreu um erro ao buscar as respostas',
+              emptyChildrenText: 'Nenhuma resposta encontrada',
+            );
+          },
+        );
+      },
     );
   }
 }
 
-class _Title extends StatelessWidget {
-  const _Title({Key key}) : super(key: key);
+class _ScoreBadge extends StatelessWidget {
+  const _ScoreBadge(this.response);
+  final Responses$Query$SymptomQuestionnaireResponses$Results response;
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
+    final scoreColor = ScoreColors.getColorFromEnum(response.score.color);
+    final containerColor = scoreColor.withOpacity(0.2);
+    final text = response.score.value.toString();
 
-    return Padding(
-      padding: EdgeInsets.fromLTRB(20, 0, 50, 0),
-      child: FittedBox(
-        child: Text(
-          'Questionários respondidos',
-          textAlign: TextAlign.left,
-          style: theme.textTheme.headline4,
-        ),
-      ),
+    return Container(
+      margin: const EdgeInsets.only(left: 8),
+      padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 12),
+      child: Text(text, style: TextStyle(color: scoreColor, fontWeight: FontWeight.w700)),
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(50), color: containerColor),
     );
   }
 }
 
-class _AnsweredQuestionnairesList extends StatelessWidget {
-  const _AnsweredQuestionnairesList({Key key}) : super(key: key);
+class _QuestionAndAnswerTile extends StatelessWidget {
+  const _QuestionAndAnswerTile({
+    Key key,
+    @required this.answerText,
+    @required this.questionText,
+  }) : super(key: key);
+
+  final String answerText;
+  final String questionText;
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
 
-    return Padding(
-      padding: EdgeInsets.fromLTRB(20, 20, 50, 0),
-      child: FittedBox(
-        child: Text(
-          'Questionários respondidos',
-          textAlign: TextAlign.left,
-          style: theme.textTheme.headline4,
-        ),
+    return ListTile(
+      contentPadding: const EdgeInsets.fromLTRB(16, 12, 16, 16),
+      dense: false,
+      enabled: true,
+      title: Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: Text(questionText, style: theme.textTheme.button),
+      ),
+      subtitle: Text(
+        answerText,
+        style: textTheme.button.copyWith(color: theme.primaryColor, fontWeight: FontWeight.w700),
       ),
     );
   }
--- lib/screens/patient/home_screen/overview/available_questionnaires.dart
+++ lib/screens/patient/home_screen/overview/available_questionnaires.dart
@@ -1,166 +1,53 @@
-import 'package:load/load.dart';
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
-import 'package:luftcare_flutter_app/helpers/error_handlers.dart';
-import 'package:luftcare_flutter_app/widgets/atoms/centered_loading_indicator.dart';
 import 'package:luftcare_flutter_app/providers/symptom_questionnaires_provider.dart';
+import 'package:luftcare_flutter_app/widgets/atoms/layout/rounded_list_tile_wrapper.dart';
+import 'package:luftcare_flutter_app/screens/patient/home_screen/overview/overview_list.dart';
 import 'package:luftcare_flutter_app/screens/patient/respond_questionnaire/respond_questionnaire_screen.dart';
 
-class AvailableQuestionnaires extends StatefulWidget {
-  const AvailableQuestionnaires({Key key, DateTime selectedDate}) : super(key: key);
+class AvailableQuestionnaires extends StatelessWidget {
+  const AvailableQuestionnaires({DateTime selectedDate});
 
-  @override
-  _AvailableQuestionnairesState createState() => _AvailableQuestionnairesState();
-}
-
-class _AvailableQuestionnairesState extends State<AvailableQuestionnaires> {
   void _openQuestionnaire(String id, BuildContext context) {
     final navigator = Navigator.of(context);
     final args = RespondQuestionnaireScreenArgs(questionnaireId: id);
     navigator.pushNamed(RespondQuestionnaireScreen.RouteName, arguments: args);
   }
 
-  Widget _buildLoadingWidget() => Padding(
-        child: CenteredLoadingIndicator(),
-        padding: EdgeInsets.fromLTRB(0, 40, 0, 30),
-      );
-
   @override
   Widget build(BuildContext context) {
-    return Query(
-      options: QueryOptions(
-        documentNode: SymptomQuestionnaires.questionnairesQueryDocumentNode,
-        fetchPolicy: FetchPolicy.cacheAndNetwork,
-      ),
-      builder: (result, {fetchMore, refetch}) {
-        if (result.loading) return _buildLoadingWidget();
-        if (result.hasException) return ErrorScreen();
+    final documentNode = SymptomQuestionnaires.questionnairesQueryDocumentNode;
+    final queryOptions = QueryOptions(documentNode: documentNode);
 
-        final response = SymptomQuestionnaires.getQuestionnairesFromQueryResult(result);
-        final questionnaires = response.results;
-        final hasNoQuestionnaires = questionnaires?.length == 0;
+    return SingleChildScrollView(
+      child: Query(
+        options: queryOptions,
+        builder: (result, {fetchMore, refetch}) {
+          final response = SymptomQuestionnaires.getQuestionnairesFromQueryResult(result);
+          final questionnaires = response?.results ?? [];
 
-        if (hasNoQuestionnaires) return _NoQuestionnaireFound(refetch: refetch);
+          final questionnaireTiles = questionnaires.map((questionnaire) {
+            final questionCount = questionnaire.questions.length;
+            final questionWord = questionCount <= 1 ? 'pergunta' : 'perguntas';
+            final trailingText = '$questionCount $questionWord';
+            final onTap = () => _openQuestionnaire(questionnaire.id, context);
 
-        final questionnairesWidgets = questionnaires
-            .map((questionnaire) => _QuestionnaireListTile(
-                  name: questionnaire.nameForPresentation,
-                  questionCount: questionnaire.questions.length,
-                  onTap: () => _openQuestionnaire(questionnaire.id, context),
-                ))
-            .toList();
+            return RoundedListTileWrapper(
+              title: Text(questionnaire.nameForPresentation),
+              trailing: Text(trailingText),
+              onTap: onTap,
+            );
+          }).toList();
 
-        return SingleChildScrollView(
-          child: Column(
-            children: [
-              _Title(),
-              ...questionnairesWidgets,
-            ],
-          ),
-        );
-      },
-    );
-  }
-}
-
-class _NoQuestionnaireFound extends StatelessWidget {
-  const _NoQuestionnaireFound({Key key, @required this.refetch}) : super(key: key);
-
-  final Future<QueryResult> Function() refetch;
-
-  @override
-  Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(30),
-          child: Text(
-            'Nenhum questionário disponível no momento',
-            textAlign: TextAlign.center,
-            style: textTheme.headline6,
-          ),
-        ),
-        FlatButton(
-          onPressed: () async {
-            showLoadingDialog(tapDismiss: false);
-            try {
-              await refetch();
-            } catch (e) {
-              print(e);
-            } finally {
-              hideLoadingDialog();
-            }
-          },
-          child: Text('Buscar novamente'),
-        ),
-      ],
-    );
-  }
-}
-
-class _QuestionnaireListTile extends StatelessWidget {
-  final String name;
-  final Function onTap;
-  final int questionCount;
-
-  const _QuestionnaireListTile({
-    Key key,
-    @required this.name,
-    @required this.onTap,
-    @required this.questionCount,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final borderRadius = BorderRadius.circular(10);
-    final whiteRoundedDecoration = BoxDecoration(
-      borderRadius: borderRadius,
-      color: Colors.white,
-    );
-
-    return Container(
-      decoration: whiteRoundedDecoration.copyWith(
-        boxShadow: [
-          BoxShadow(
-            blurRadius: 5,
-            offset: Offset.fromDirection(90, 3),
-            color: Theme.of(context).primaryColor.withOpacity(0.2),
-          ),
-        ],
-      ),
-      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 6),
-      child: Material(
-        type: MaterialType.transparency,
-        child: Ink(
-          decoration: whiteRoundedDecoration,
-          child: ListTile(
-            onTap: onTap,
-            title: Text(name),
-            trailing: Text('$questionCount pergunta(s)'),
-            shape: RoundedRectangleBorder(borderRadius: borderRadius),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class _Title extends StatelessWidget {
-  const _Title({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-
-    return Padding(
-      padding: EdgeInsets.fromLTRB(20, 0, 50, 10),
-      child: FittedBox(
-        child: Text(
-          'Questionários disponíveis',
-          textAlign: TextAlign.left,
-          style: theme.textTheme.headline4,
-        ),
+          return OverviewList(
+            title: 'Questionários disponíveis',
+            children: questionnaireTiles,
+            hasError: result.hasException,
+            isLoading: result.loading,
+            errorText: 'Ocorreu um erro ao buscar os questionários',
+            emptyChildrenText: 'Nenhum questionário encontrado',
+          );
+        },
       ),
     );
   }
